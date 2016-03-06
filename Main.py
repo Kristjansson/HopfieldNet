f89924b1520e1b6f8236c6ac0a8e516030ec8fa1
Joe <joseph.k.christianson@gmail.com>
2016-03-05 23:46:37 -0800

    initial commit

---
+++ Main.py
@@ -0,0 +1,43 @@
+from keras.datasets import mnist
+import random
+import numpy as np
+
+
+class HopfieldNet:
+    @staticmethod
+    def biasFunc(updateVal):
+        return 1 if updateVal > 0 else -1
+
+    def __init__(self,size):
+        self.size = size
+        self.weights = np.random.rand(self.size, self.size)
+        self.neurons = np.zeros(self.size, 1)
+
+    def input(self, input):
+        self.neurons = input
+
+    #check that an input call has been made first
+    def singleIteration(self, neuronNum):
+        # temp = np.append(self.neurons[0:neuronNum], self.neurons[neuronNum+1:])
+        temp = self.neurons
+        temp[neuronNum] = 0
+        # updateVal = sum(self.weights[0:, neuronNum]*temp)
+        # self.neurons[neuronNum] = 1 if updateVal > 0 else -1
+        self.neurons[neuronNum] = self.biasFunc(sum(self.weights[0:, neuronNum]*temp))
+
+    def fullIteration(self):
+        temp = random.shuffle(np.linspace(0,self.size-1,num=self.size))
+        for idx in temp:
+            self.singleIteration(idx)
+
+    def learnPattern(self, pattern):
+        for outerIndex in range(0,self.size):
+            for innerIndex in range(0,self.size):
+                if outerIndex != innerIndex:
+                    val = (2*pattern[outerIndex]-1)*(2*pattern[outerIndex]-1)
+                    self.weights[outerIndex, innerIndex] += val
+                    self.weights[innerIndex, outerIndex] += val
+
+
+#Main
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
